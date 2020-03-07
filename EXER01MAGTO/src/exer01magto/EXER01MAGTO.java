1983a92a94c258217f77e96d6b36a1e163ee0c85
dresfertgunient <61903573+dresfertgunient@users.noreply.github.com>
2020-03-07 17:44:45 +0800

    Add files via upload

---
+++ EXER01MAGTO/src/exer01magto/EXER01MAGTO.java
@@ -0,0 +1,16 @@
+/*
+ *Ron Christian Y. Magto
+ * To display full name
+ * 8/14/19
+ */
+package exer01magto;
+
+
+public class EXER01MAGTO {
+
+    
+    public static void main(String[] args) {
+     System.out.println("Ron Christian Y. Magto");
+    }
+    
+}
