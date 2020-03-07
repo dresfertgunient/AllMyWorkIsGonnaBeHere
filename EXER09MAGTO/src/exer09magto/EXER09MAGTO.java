a4250cf0ea59e3a843d695b199855bb2d4fb8722
dresfertgunient <61903573+dresfertgunient@users.noreply.github.com>
2020-03-07 18:01:19 +0800

    Add files via upload

---
+++ EXER09MAGTO/src/exer09magto/EXER09MAGTO.java
@@ -0,0 +1,23 @@
+/*
+ * Ron Christian Y. Magto
+ * Exercise 9
+ *9/19?19
+ */
+package exer09magto;
+
+ import java.util.*;
+public class EXER09MAGTO {
+
+  
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int num [] = new int [100];
+        System.out.println("Enter an integer");
+        num[1]=console.nextInt();
+        for (int x =0;  x<=100; x++){
+         System.out.println(num[1]);
+        }
+     
+    }
+      
+}
